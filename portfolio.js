dfaba37684d5b6c83fd38c038e6164110dd3bc6b
yates-nikhil <nikhilyy@my.yorku.ca>
2023-06-18 16:16:43 -0400

    mobile opt temp

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,18 @@
-window.addEventListener('scroll', () => {
-    document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-}, false);
+// window.addEventListener('scroll', () => {
+//     document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+// }, false);
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("hamburger-menu").style.top = "60px";
+  } else {
+    document.getElementById("hamburger-menu").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+
 
