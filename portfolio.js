76cdb2818e8bf5f57a7009bd4e8adf4797eb1baa
yates-nikhil <nikhilyy@my.yorku.ca>
2023-10-17 00:05:03 -0400

    mobileOpt started

--- portfolio.js
+++ portfolio.js
@@ -2,6 +2,28 @@
 //     document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
 // }, false);
 
+function projectsGit(projname) {
+
+    document.createElement
+
+}
+
+flag = 0;
+
+function infoSlider(id, overlayId) {
+
+    if (flag === 0) {
+        document.getElementById(overlayId).style.height = '50%';
+        document.getElementById(id).style.width = '250px';
+        flag = 1;
+    }
+    else {
+        document.getElementById(overlayId).style.height = '0';
+        document.getElementById(id).style.width = '200px';
+        flag = 0;
+    }
+
+}
 
 
 
